8a29997a8def75387bfd7cd5787b03f521a6ea4a
Ahmadalhomsi <tarikuka.aa@gmail.com>
2024-10-02 17:55:35 +0300

    Customers REST api (all)

---
+++ app/api/customers/route.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function POST(req) {
+    const { name, email, phone } = await req.json();
+    try {
+        const customer = await prisma.customer.create({
+            data: {
+                name,
+                email,
+                phone
+            },
+        });
+        return NextResponse.json(customer, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to create customer' }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        const customers = await prisma.customer.findMany();
+        return NextResponse.json(customers, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to fetch customers' }, { status: 500 });
+    }
+}
+
+
